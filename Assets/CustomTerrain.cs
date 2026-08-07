bb0e5d39874d123faaaad84031d8cf5dc80518e1
feedoria <pirsan.victoria@yahoo.com>
2026-08-07 16:10:31 +0300

    -- implemented random heights -> simple way

--- Assets/CustomTerrain.cs
+++ Assets/CustomTerrain.cs
@@ -9,9 +9,29 @@ public class CustomTerrain : MonoBehaviour
 {
     public Vector2 randomHeightRange = new Vector2(0, 0.1f); // the maximum and minimum hight
     // 0.1f is a rlly large terrain height despite of how it appears on first look
+    public Terrain terrain;
+    public TerrainData terrainData;
     public void RandomTerrain()
     {
-        
+        float [,] heightMap = terrainData.GetHeights(0, 0, terrainData.heightmapResolution, terrainData.heightmapResolution);
+        // getting the data out of the terrain and putting it into heightMap
+        for (int x = 0; x < terrainData.heightmapResolution; x++)
+        {
+            for (int z = 0; z < terrainData.heightmapResolution; z++)
+            {
+                // x = weight , z = depth 
+                heightMap[x, z] += UnityEngine.Random.Range(randomHeightRange.x, randomHeightRange.y);
+            }
+        }
+        terrainData.SetHeights(0, 0, heightMap); // setting it back into the terrain
+    }
+
+    void OnEnable()
+    {
+        Debug.Log("Initializing terrain data");
+        terrain = this.GetComponent<Terrain>();
+        terrainData = Terrain.activeTerrain.terrainData; // Terrain.activeTerrain = global static class 
+        // i can also have Terrain.terrainData if i ve got more than one terrain in my scene
     }
     void Start()
     {
